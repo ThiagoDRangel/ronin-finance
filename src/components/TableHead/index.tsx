1c3163a3c15175fb990dd4fa228293d1b4736048
ThiagoDRangel <info.tec.campos@gmail.com>
2024-08-03 18:03:16 -0300

    feat: mobile version

--- src/components/TableHead/index.tsx
+++ src/components/TableHead/index.tsx
@@ -7,10 +7,10 @@ function TableHead () {
         <th className="table-title">Earn Control</th>
       </tr>
       <tr>
-        <th className="item">Homeland(AXS)</th>
-        <th className="item">Katana(USDC-RON)</th>
-        <th className="item">Katana(AXS-RON)</th>
-        <th className="item">Katana(SLP-RON)</th>
+        <th className="item">Homeland (AXS)</th>
+        <th className="item">Katana (USDC-RON)</th>
+        <th className="item">Katana (AXS-RON)</th>
+        <th className="item">Katana (SLP-RON)</th>
         <th className="item">RON Staking</th>
         <th className="item">AXS Staking</th>
       </tr>
