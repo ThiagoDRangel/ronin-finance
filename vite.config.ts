46ed4af072198336dd426094a3f989706c7bae89
ThiagoDRangel <info.tec.campos@gmail.com>
2024-08-03 14:20:25 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-// vite.config.ts ou vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  optimizeDeps: {
-    include: ['jspdf', 'jspdf-autotable']
-  },
+  plugins: [react()],
 });
